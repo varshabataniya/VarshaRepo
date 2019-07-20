d63e4d0027ce333bc223293227448ffe58a8f6e2
varshabataniya <varshabataniya@gmail.com>
2019-07-20 15:40:54 +0530

    added book.java

---
+++ book.java
@@ -0,0 +1,22 @@
+public class Book{
+	private it bookId;
+	private String title;
+	private String author;
+	private float price;
+
+	public void setBookId(int bookId){
+	this.bookId = bookId;
+	}
+	public int getBookId(){
+        return bookId;
+	}
+
+	public void setTitle(int title){
+	this.title = title;
+	}
+	public void setAuthor(String author){
+	this.author = author;
+	}
+
+
+}
