4f26d0a30cf70b43351e4362fc125c4d0ecdc841
varshabataniya <varshabataniya@gmail.com>
2019-07-20 11:31:39 +0530

    update file

--- book.java
+++ book.java
@@ -2,5 +2,13 @@ public class Book
 {
    private int bookId;
    private String bookName;
-   
+   private author;
+   private float price;
+
+   public void setBookId(int bookId){
+     this.bookId = bookId;   
+     }
+public int getBookId(){
+	return bookId;
+	}
 }
